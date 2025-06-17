c2f30c932c19f0dd3a12e2df0a456a019d53f2bc
Pedro Lima <tsc.pedrolima@gmail.com>
2025-06-17 14:30:28 -0300

    feat: add footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 import AddTaskForm from "@/components/dashboard/AddTaskForm";
 import Header from "@/components/dashboard/Header";
 import TasksList from "@/components/dashboard/TasksList";
+import Footer from "../components/commons/Footer";
 
 export default function Dashboard() {
   return (
-    <div>
-      <main className="min-h-screen px-4 py-10">
+    <div className="min-h-screen px-4 py-8">
+      <main className="flex flex-col flex-1 items-center justify-center">
         <Header />
         <AddTaskForm />
         <TasksList />
       </main>
-      <footer></footer>
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/commons/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-center text-gray-light text-sm py-4">
+      <p>Desenvolvido por Pedro Lima • Inspirado no site da ALUCOM</p>
+    </footer>
+  );
+}
